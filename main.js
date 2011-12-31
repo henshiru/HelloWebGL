044bc4de1d5f17cffb5ad19854b912e425f8bf3a
henshiru <henshiru@gmail.com>
2011-12-31 16:03:58 +0900

    Add lighting and some refactoring

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-var gl = null;
-var program = null;
-var canvas = null;
-var buffer = null;
-
-function onLoad() {
-  canvas = document.getElementById("canvas_main");
-  gl = glutil.getGlContext(canvas);
+function init() {
+  var canvas = document.getElementById("canvas_main");
+  var gl = glutil.getGlContext(canvas);
   if (!gl) {
     alert("Unable to initialize WebGL. Your browser may not support it.");
     return;
@@ -13,51 +8,67 @@ function onLoad() {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.enable(gl.DEPTH_TEST);
   gl.depthFunc(gl.LEQUAL);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.cullFace(gl.BACK);
   gl.enable(gl.CULL_FACE);
 
-  program = glutil.createProgram(gl, "vshader", "fshader");
+  var program = glutil.createProgram(gl, "vshader", "fshader");
   gl.useProgram(program);
 
-  buffer = gl.createBuffer();
+  var buffer = gl.createBuffer();
 
-  setInterval(drawScene, 15);
-}
+  var drawScene = function() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-function drawScene() {
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.uniform1f(gl.getUniformLocation(program, "ambient"), 0.2);
+    gl.uniform3f(gl.getUniformLocation(program, "directional_light"),
+                 0, 0.5, 0);
 
-  var locTransform = gl.getUniformLocation(program, "transform");
-  var matrix = transform.identity();
-  matrix = transform.multiply(transform.rotateY(Math.PI*2*((new Date).getTime())/2000), matrix);
-  matrix = transform.multiply(transform.translate(0, 0, -4), matrix);
-  matrix = transform.multiply(transform.perspective(Math.PI/3, canvas.width/canvas.height, 1, 10), matrix);
-  gl.uniformMatrix4fv(locTransform, false, transform.toFloat32Array(matrix));
+    var perspective = transform.perspective(
+        Math.PI/3, canvas.width/canvas.height, 1, 10);
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "projection"), false,
+                        perspective.toFloat32Array());
 
-  var vertices = [  
-      1.0,  1.0,  0.0,
-      -1.0, 1.0,  0.0,
-      1.0,  -1.0, 0.0,
-      -1.0, -1.0, 0.0
-  ];
-  var colors = [
-      1.0, 0.0, 0.0,
-      0.0, 1.0, 0.0,
-      0.0, 0.0, 1.0,
-      0.0, 0.0, 0.0,
-  ];
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);  
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices.concat(colors)), gl.STATIC_DRAW);
+    var modelview = transform.translate(0, 0, -4).multiply(
+        transform.rotateX(Math.PI*2*((new Date).getTime())/2000));
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelview"), false,
+                        modelview.toFloat32Array());
+
+    var vertices = [1.0,  1.0,  0.0,
+                    -1.0, 1.0,  0.0,
+                    1.0,  -1.0, 0.0,
+                    -1.0, -1.0, 0.0,
+                    ];
+    var colors = [1.0, 0.0, 0.0,
+                  0.0, 1.0, 0.0,
+                  0.0, 0.0, 1.0,
+                  0.0, 0.0, 0.0,
+                  ];
+    var normals = [0.0, 0.0, 1.0,
+                   0.0, 0.0, 1.0,
+                   0.0, 0.0, 1.0,
+                   0.0, 0.0, 1.0,
+                   ];
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);  
+    gl.bufferData(gl.ARRAY_BUFFER,
+                  new Float32Array(vertices.concat(colors).concat(normals)),
+                  gl.STATIC_DRAW);
+
+    var N = vertices.length/3;
   
-  var locPosition = gl.getAttribLocation(program, "position");
-  gl.vertexAttribPointer(locPosition, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(locPosition);
+    var locPosition = gl.getAttribLocation(program, "position");
+    gl.vertexAttribPointer(locPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(locPosition);
 
-  var locColor = gl.getAttribLocation(program, "color");
-  gl.vertexAttribPointer(locColor, 3, gl.FLOAT, false, 0, vertices.length*4);
-  gl.enableVertexAttribArray(locColor);
+    var locColor = gl.getAttribLocation(program, "color");
+    gl.vertexAttribPointer(locColor, 3, gl.FLOAT, false, 0, N*3*4);
+    gl.enableVertexAttribArray(locColor);
+
+    var locNormal = gl.getAttribLocation(program, "normal");
+    gl.vertexAttribPointer(locNormal, 3, gl.FLOAT, false, 0, N*3*2*4);
+    gl.enableVertexAttribArray(locNormal);
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  };
   
-
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  setInterval(drawScene, 16);
 }
--- transform.js
+++ transform.js
@@ -1,95 +1,145 @@
+var transform = function(){
+  var that = {};
 
-var transform = {};
+  var vector = function(x, y, z) {
+    x = x || 0;
+    y = y || 0;
+    z = z || 0;
+    var v = [x, y, z];
+    v.copy = function() {
+      return vector(v[0], v[1], v[2]);
+    };
+    v.abs2 = function() {
+      return v[0]*v[0] + v[1]*v[1] + v[2]*v[2];
+    };
+    v.abs = function() {
+      return Math.sqrt(v.abs2());
+    };
+    v.dot = function(v2) {
+      return v[0]*v2[0] + v[1]*v2[1] + v[2]*v2[2];
+    };
+    v.cross = function(v2) {
+      return vector(v[1]*v2[2] - v[2]*v2[1],
+                    v[2]*v2[0] - v[0]*v2[2],
+                    v[0]*v2[1] - v[1]*v2[0]);
+    };
+    v.multiply = function(k) {
+      v[0] *= k;
+      v[1] *= k;
+      v[2] *= k;
+      return v;
+    };
+    v.add = function(v2) {
+      v[0] += v2[0];
+      v[1] += v2[1];
+      v[2] += v2[2];
+      return v;
+    };
+    v.normalize = function() {
+      return v.multiply(1.0/v.abs());
+    };
+    return v;
+  };
+  that.vector = vector;
+ 
+  var matrix = function() {
+    var m = [[0, 0, 0, 0],
+             [0, 0, 0, 0],
+             [0, 0, 0, 0],
+             [0, 0, 0, 0]];
 
-transform.zero = function() {
-  return [[0, 0, 0, 0],
-          [0, 0, 0, 0],
-          [0, 0, 0, 0],
-          [0, 0, 0, 0]];
-};
+    var multiply = function(mat2) {
+      var t = matrix();
+      var i, j;
+      for (i = 0; i != 4; ++i) {
+        for (j = 0; j != 4; ++j) {
+          t[i][j] = m[i][0]*mat2[0][j] + m[i][1]*mat2[1][j] +
+              m[i][2]*mat2[2][j] + m[i][3]*mat2[3][j];
+        }
+      }
+      return t;
+    };
+    var toFloat32Array = function() {
+      var a = new Float32Array(16);
+      var i, j;
+      for (i = 0; i != 4; ++i) {
+        for (j = 0; j != 4; ++j) {
+          a[j*4 + i] = m[i][j];
+        }
+      }
+      return a;
+    };
+    m.multiply = multiply;
+    m.toFloat32Array = toFloat32Array;
+    return m;
+  };
+  that.matrix = matrix;
+  that.zero = that.matrix;
 
-transform.identity = function() {
-  var m = transform.zero();
-  m[0][0] = m[1][1] = m[2][2] = m[3][3] = 1;
-  return m;
-};
+  that.identity = function() {
+    var m = that.zero();
+    m[0][0] = m[1][1] = m[2][2] = m[3][3] = 1;
+    return m;
+  };
 
-transform.perspective = function(fovy, aspect, nearZ, farZ) {
-  var frust = transform.zero();
-  var tan_fovy = Math.tan( fovy );
-  frust[0][0] = 1 / (aspect*tan_fovy);
-  frust[1][1] = 1 / tan_fovy;
-  frust[2][2] = -(nearZ + farZ) / (farZ - nearZ);
-  frust[2][3] = -2.0 * nearZ * farZ / (farZ - nearZ);
-  frust[3][2] = -1.0;
-  return frust;
-};
+  that.perspective = function(fovy, aspect, nearZ, farZ) {
+    // See http://www.songho.ca/opengl/gl_projectionmatrix.html for details.
+    var m = that.zero();
+    var tan_fovy = Math.tan( fovy );
+    m[0][0] = 1 / (aspect*tan_fovy);
+    m[1][1] = 1 / tan_fovy;
+    m[2][2] = -(nearZ + farZ) / (farZ - nearZ);
+    m[2][3] = -2.0 * nearZ * farZ / (farZ - nearZ);
+    m[3][2] = -1.0;
+    return m;
+  };
 
-transform.multiply = function(mat1, mat2) {
-  var m = transform.zero();
-  var i, j;
-  for (i = 0; i != 4; ++i) {
-    for (j = 0; j != 4; ++j) {
-      m[i][j] = mat1[i][0]*mat2[0][j] + mat1[i][1]*mat2[1][j] + mat1[i][2]*mat2[2][j] + mat1[i][3]*mat2[3][j];
-    }
-  }
-  return m;
-};
+  that.rotateX = function(t) {
+    var m = that.zero();
+    var s = Math.sin(t);
+    var c = Math.cos(t);
+    m[0][0] = 1;
+    m[1][1] = c;
+    m[1][2] = -s;
+    m[2][1] = s;
+    m[2][2] = c;
+    m[3][3] = 1;
+    return m;
+  };
 
-transform.rotateX = function(t) {
-  var m = transform.zero();
-  var s = Math.sin(t);
-  var c = Math.cos(t);
-  m[0][0] = 1;
-  m[1][1] = c;
-  m[1][2] = -s;
-  m[2][1] = s;
-  m[2][2] = c;
-  m[3][3] = 1;
-  return m;
-};
+  that.rotateY = function(t) {
+    var m = that.zero();
+    var s = Math.sin(t);
+    var c = Math.cos(t);
+    m[0][0] = c;
+    m[0][2] = s;
+    m[1][1] = 1;
+    m[2][0] = -s;
+    m[2][2] = c;
+    m[3][3] = 1;
+    return m;
+  };
 
-transform.rotateY = function(t) {
-  var m = transform.zero();
-  var s = Math.sin(t);
-  var c = Math.cos(t);
-  m[0][0] = c;
-  m[0][2] = s;
-  m[1][1] = 1;
-  m[2][0] = -s;
-  m[2][2] = c;
-  m[3][3] = 1;
-  return m;
-};
+  that.rotateZ = function(t) {
+    var m = that.zero();
+    var s = Math.sin(t);
+    var c = Math.cos(t);
+    m[0][0] = c;
+    m[0][1] = -s;
+    m[1][0] = s;
+    m[1][1] = c;
+    m[2][2] = 1;
+    m[3][3] = 1;
+    return m;
+  };
 
-transform.rotateZ = function(t) {
-  var m = transform.zero();
-  var s = Math.sin(t);
-  var c = Math.cos(t);
-  m[0][0] = c;
-  m[0][1] = -s;
-  m[1][0] = s;
-  m[1][1] = c;
-  m[2][2] = 1;
-  m[3][3] = 1;
-  return m;
-};
+  that.translate = function(x, y, z) {
+    var m = that.identity();
+    m[0][3] = x;
+    m[1][3] = y;
+    m[2][3] = z;
+    return m;
+  };
 
-transform.translate = function(x, y, z) {
-  var m = transform.identity();
-  m[0][3] = x;
-  m[1][3] = y;
-  m[2][3] = z;
-  return m;
-};
-
-transform.toFloat32Array = function(m) {
-  var a = new Float32Array(16);
-  var i, j;
-  for (i = 0; i != 4; ++i) {
-    for (j = 0; j != 4; ++j) {
-      a[j*4 + i] = m[i][j];
-    }
-  }
-  return a;
-};
+  return that;
+}();
